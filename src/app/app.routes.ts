12925e93e6551f636bd536928172a110159bd49d
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2025-04-21 15:21:40 +0200

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./gifs/pages/dashboard-page/dashboard-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+    },
+
+    {
+        path: 'trending',
+        loadComponent: () =>
+            import('./gifs/pages/trending-page/trending-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+    },
+
+    {
+        path: 'search',
+        loadComponent: () =>
+            import('./gifs/pages/search-page/search-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+    },
+
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    }
+];
