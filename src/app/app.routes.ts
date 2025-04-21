e3f6b2d5ff4f4007da713343333680d346171b82
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2025-04-21 17:00:21 +0200

    Cambios en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,30 @@ export const routes: Routes = [
         path: 'dashboard',
         loadComponent: () =>
             import('./gifs/pages/dashboard-page/dashboard-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+        children: [
+
+            {
+                path: 'trending',
+                loadComponent: () =>
+                    import('./gifs/pages/trending-page/trending-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+            },
+        
+            {
+                path: 'search',
+                loadComponent: () =>
+                    import('./gifs/pages/search-page/search-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
+            },
+
+
+            {
+                path: '**',
+                redirectTo: 'trending'
+            }
+
+        ]
     },
 
-    {
-        path: 'trending',
-        loadComponent: () =>
-            import('./gifs/pages/trending-page/trending-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
-    },
-
-    {
-        path: 'search',
-        loadComponent: () =>
-            import('./gifs/pages/search-page/search-page.component'),   //es necesario añadir export default en el componente para manejar la promesa.
-    },
+    
 
     {
         path: '**',
--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    SideMenuComponent
+],
   templateUrl: './dashboard-page.component.html',
 
 })
